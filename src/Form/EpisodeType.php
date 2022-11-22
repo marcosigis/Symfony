a56a6d77424fa59a727e35841c6d9ba4d8a762be
MARCO SIGISMONDI <marco.sigismondi@hotmail.com>
2022-11-22 23:03:01 +0100

    missing js part from symfony doc

--- src/Form/EpisodeType.php
+++ src/Form/EpisodeType.php
@@ -3,16 +3,19 @@
 namespace App\Form;
 
 use App\Entity\Episode;
-use App\Entity\Season;
 use App\Entity\Program;
-use App\Repository\ProgramRepository;
+use App\Entity\Season;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 
 class EpisodeType extends AbstractType
 {
+
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
@@ -24,11 +27,49 @@ class EpisodeType extends AbstractType
                 'class' => Program::class,
                 'mapped' => false,
                 'choice_label' => 'title'
-            ])
-            ->add('season', EntityType::class, [
-                'class' => Season::class,
-                'choice_label' => 'number',
             ]);
+        //QUA FUNZIONA TUTTO
+        // ->add('season', EntityType::class, [
+        //     'class' => Season::class,
+        //     'choice_label' => 'number',
+        // ]);
+        $formModifier = function (FormInterface $form, Program $program = null) {
+            $seasons = null === $program ? [] : $program->getSeasons();
+
+            $form->add('season', EntityType::class, [
+                'class' => Season::class,
+                'placeholder' => '',
+                'choices' => $seasons,
+            ]);
+        };
+
+        $builder->addEventListener(
+            FormEvents::PRE_SET_DATA,
+            function (FormEvent $event) use ($formModifier) {
+                //$form = $event->getForm();
+
+                // this would be your entity Episode
+                $data = $event->getData();
+
+                //$program = $data->getTitle();
+                $formModifier($event->getForm(), $data->getTitle());
+                // dd($program);
+
+            }
+        );
+
+        $builder->get('program')->addEventListener(
+            FormEvents::POST_SUBMIT,
+            function (FormEvent $event) use ($formModifier) {
+                // It's important here to fetch $event->getForm()->getData(), as
+                // $event->getData() will get you the client data (that is, the ID)
+                $program = $event->getForm()->getData();
+
+                // since we've added the listener to the child, we'll have to pass on
+                // the parent to the callback function!
+                $formModifier($event->getForm()->getParent(), $program);
+            }
+        );
     }
 
     public function configureOptions(OptionsResolver $resolver): void
